6ac07f1575167d545e8ee0d5fb84f9c648e4a146
Manmeet Singh <157360398+manmeetsingh1115@users.noreply.github.com>
2026-07-07 13:42:44 +1000

    Add 1.1P source file

---
+++ 1.1P/source-files/index.js
@@ -0,0 +1,8 @@
+console.log("Welcome to SIT725 Applied Software Engineering");
+console.log("This is my Task 1.1P introduction to programming activity");
+
+let studentName = "Manmeet Singh";
+let unitCode = "SIT725";
+
+console.log("Student Name: " + studentName);
+console.log("Unit Code: " + unitCode);
